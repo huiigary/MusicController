a15b58960d768b5b9aa39e687b129e2e3ea05b1b
garyhui <gary.hui@sendum.com>
2023-03-01 12:08:10 -0800

    Created simple create room page to pause, votes required to skip, and create room

--- frontend/src/components/CreateRoom.jsx
+++ frontend/src/components/CreateRoom.jsx
@@ -1,8 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+  Grid,
+  Typography,
+  Button,
+  FormControl,
+  FormHelperText,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+  TextField,
+  Link,
+} from '@mui/material'
 
 export function CreateRoom(props) {
-  
+  const defaultVotes = 2
 
-  
-  return <h1> Create Room Page</h1>
+  const [guestCanPause, setGuestVote] = useState(true)
+  const [votesToSkip, setVotesToSkip] = useState(defaultVotes)
+
+  const handleVotesChange = (e) => {
+    console.log('handle votes changing:', e.target.value)
+    setVotesToSkip(e.target.value)
+  }
+
+  const handleGuestCanPauseChange = (e) => {
+    console.log('clicked guests can pause', e.target.value)
+    setGuestVote(e.target.value === 'true' ? true : false)
+  }
+
+  const handleRoomButtonClicked = () => {
+    console.log('button room clicked')
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        votes_to_skip: votesToSkip,
+        guests_can_pause: guestCanPause, // Note: the body key (votes_to_skip... etc) must match how it is written on the backend side
+      }),
+    }
+
+    // send the request to this endpoint--> then convert to json
+    fetch('/api/create-room', requestOptions).then((response) =>
+      response.json().then((data) => console.log('create Room data:', { data }))
+    )
+  }
+
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align='center'>
+        <Typography component='h4' variant='h4'>
+          Create Room
+        </Typography>
+      </Grid>
+      {/* To control playback */}
+      <Grid item xs={12} align='center'>
+        <FormControl component='fieldset'>
+          <FormHelperText>
+            <div algin='center'> Guest Control of Playback State</div>
+          </FormHelperText>
+          <RadioGroup
+            row
+            defaultValue={true}
+            onChange={(e) => handleGuestCanPauseChange(e)}
+          >
+            <FormControlLabel
+              value={true}
+              control={<Radio color='primary'></Radio>}
+              label='Play/Pause'
+              labelPlacement='bottom'
+            ></FormControlLabel>
+            <FormControlLabel
+              value={false}
+              control={<Radio color='secondary'></Radio>}
+              label='No control'
+              labelPlacement='bottom'
+            ></FormControlLabel>
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      {/* The number of people to skip song */}
+      <Grid item xs={12} align='center'>
+        <FormControl algin='center'>
+          <TextField
+            required={true}
+            type='number'
+            defaultValue={defaultVotes}
+            onChange={(e) => handleVotesChange(e)}
+            inputProps={{
+              min: 1,
+              style: { textAlign: 'center' },
+            }}
+          ></TextField>
+          <FormHelperText>
+            <div algin='center'>Votes required to skip</div>
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      {/* Buttons to submit/goBack ... component will be like a link*/}
+      <Grid xs={12} item align='center'>
+        <Button
+          color='primary'
+          variant='outlined'
+          onClick={() => handleRoomButtonClicked()}
+        >
+          Create a Room
+        </Button>
+        <Button color='secondary' variant='contained' href='/' component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  )
 }
